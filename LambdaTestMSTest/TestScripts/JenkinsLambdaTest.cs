1db75395259d90200a335d7e29321496d6de0e0e
qa-repo <43996655+qa-repo@users.noreply.github.com>
2018-12-20 21:28:46 +0530

    Added sample code for C# MSTest framework
    Added sample code for C# MSTest framework

---
+++ LambdaTestMSTest/TestScripts/JenkinsLambdaTest.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Configuration;
+
+namespace LambdaTestMSTest.TestScripts
+{
+
+    [TestClass]
+    public class JenkinsLambdaTest
+    {
+        private IWebDriver driver;
+        protected String ltUserName;
+        protected String ltAppKey;
+        protected String plateform;
+        protected String browser;
+        protected String browserVersion;
+
+        
+        [TestInitialize]
+        public void setUp()
+        {
+            this.init();
+             DesiredCapabilities caps1 = new DesiredCapabilities();
+            caps1.SetCapability("platform", plateform);
+            caps1.SetCapability("browserName", browser);
+            caps1.SetCapability("version", browserVersion);
+            caps1.SetCapability("user", ltUserName);
+            caps1.SetCapability("accessKey", ltAppKey);
+            caps1.SetCapability("network", true); // To enable network logs
+            caps1.SetCapability("visual", true); // To enable step by step screenshot
+            caps1.SetCapability("video", true); // To enable video recording
+            caps1.SetCapability("console", true); // To capture console logs
+            driver = new RemoteWebDriver(new Uri(ConfigurationSettings.AppSettings["LTUrl"]), caps1, TimeSpan.FromSeconds(600));
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://4dvanceboy.github.io/lambdatest/lambdasampleapp.html";
+            Assert.AreEqual("Sample page - lambdatest.com", driver.Title);
+        }
+
+        [TestMethod]
+        public void TestJenkins()
+        {
+            String itemName = "Yey, Let's add it to list";
+
+            // Click on First Check box 
+            IWebElement firstCheckBox = driver.FindElement(By.Name("li1"));
+            firstCheckBox.Click();
+
+            // Click on Second Check box 
+            IWebElement secondCheckBox = driver.FindElement(By.Name("li2"));
+            secondCheckBox.Click();
+
+            // Enter Item name	
+            IWebElement textfield = driver.FindElement(By.Id("sampletodotext"));
+            textfield.SendKeys(itemName);
+
+            // Click on Add button 
+            IWebElement addButton = driver.FindElement(By.Id("addbutton"));
+            addButton.Click();
+
+            // Verified Added Item name
+            IWebElement itemtext = driver.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span"));
+            String getText = itemtext.Text;
+            Assert.IsTrue(itemName.Contains(getText));
+        }
+
+        [TestCleanup]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+        public void init()
+        {
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("LT_USERNAME"))){
+                this.ltUserName = ConfigurationSettings.AppSettings["LTUser"];
+            }
+
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("LT_APPKEY")))
+                  this.ltAppKey = ConfigurationSettings.AppSettings["LTAccessKey"];
+
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("LT_OPERATING_SYSTEM")))
+                this.plateform = ConfigurationSettings.AppSettings["OS"];
+
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("LT_BROWSER")))
+                this.browser = ConfigurationSettings.AppSettings["Browser"];
+
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("LT_BROWSER_VERSION")))
+                this.browserVersion = ConfigurationSettings.AppSettings["BrowserVersion"];
+
+
+        }
+
+    }
+}
---
+++ LambdaTestMSTest/TestScripts/LocalTest.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaTestMSTest.TestScripts
+{
+    [TestClass]
+    public class AdvanceBoyTest 
+	{
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Open()
+        {
+            String LTUser = ConfigurationSettings.AppSettings["LTUser"];
+            String LTAccessKey = ConfigurationSettings.AppSettings["LTAccessKey"];
+
+            DesiredCapabilities capsLT = new DesiredCapabilities();
+            capsLT.SetCapability("platform", ConfigurationSettings.AppSettings["OS"]);
+            capsLT.SetCapability("browserName", ConfigurationSettings.AppSettings["Browser"]);
+            capsLT.SetCapability("version", ConfigurationSettings.AppSettings["BrowserVersion"]);
+            capsLT.SetCapability("user", LTUser);
+            capsLT.SetCapability("accessKey", LTAccessKey);
+            capsLT.SetCapability("build", "LambdaTestSampleApp");
+            capsLT.SetCapability("name", "LambdaTestJavaSample");
+            capsLT.SetCapability("network", true); // To enable network logs
+            capsLT.SetCapability("visual", true); // To enable step by step screenshot
+            capsLT.SetCapability("video", true); // To enable video recording
+            capsLT.SetCapability("console", true); // To capture console logs
+            driver = new RemoteWebDriver(new Uri(ConfigurationSettings.AppSettings["LTUrl"]), capsLT, TimeSpan.FromSeconds(600)); ;
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://4dvanceboy.github.io/lambdatest/lambdasampleapp.html";
+            Assert.AreEqual("Sample page - lambdatest.com", driver.Title);
+        }
+
+
+        [TestMethod]
+        public void TestLocal()
+		{				
+			String itemName = "Yey, Let's add it to list";
+
+			// Click on First Check box 
+			IWebElement firstCheckBox = driver.FindElement(By.Name("li1"));
+			firstCheckBox.Click();
+
+			// Click on Second Check box 
+			IWebElement secondCheckBox = driver.FindElement(By.Name("li2"));
+			secondCheckBox.Click();
+
+			// Enter Item name	
+			IWebElement textfield = driver.FindElement(By.Id("sampletodotext"));
+			textfield.SendKeys(itemName);
+
+			// Click on Add button 
+			IWebElement addButton = driver.FindElement(By.Id("addbutton"));
+			addButton.Click();
+
+			// Verified Added Item name
+			IWebElement itemtext = driver.FindElement(By.XPath("/html/body/div/div/div/ul/li[6]/span"));
+			String getText = itemtext.Text;
+			Assert.IsTrue(itemName.Contains(getText));
+		}
+
+        [TestCleanup]
+        public void Close()
+		{
+			driver.Quit();
+		}
+	}
+}
